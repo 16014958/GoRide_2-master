09c76a60dbfa8cb08a75f066b740babbb4fecc98
Callum <callumradford26@gmail.com>
2018-08-19 10:06:38 +1200

    final webpage

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def index():
 def landing():
   trails = []
   con = sqlite3.connect(MENUDB)
-  cur = con.execute('SELECT DISTINCT style, img FROM links')
+  cur = con.execute('SELECT DISTINCT style FROM rides')
   for row in cur:
     trails.append(list(row))
 
@@ -124,6 +124,49 @@ def Westlocation():
   ride = []
   con = sqlite3.connect(MENUDB)
   cur = con.execute('SELECT DISTINCT description, trail , img, difficulty, distance, style, location FROM rides WHERE location="West Coast"')
+
+  for row in cur:
+    ride.append(list(row))
+
+  return render_template('result.html', ride=ride)
+
+
+
+
+
+
+@app.route('/Heaphy/result')
+def Heaphy():
+  ride = []
+  con = sqlite3.connect(MENUDB)
+  cur = con.execute('SELECT DISTINCT description, trail , img, difficulty, distance, style, location FROM rides WHERE trail="The Heaphy track"')
+
+  for row in cur:
+    ride.append(list(row))
+
+  return render_template('result.html', ride=ride)
+
+
+
+
+@app.route('/Wakamarina/result')
+def Wakamarina():
+  ride = []
+  con = sqlite3.connect(MENUDB)
+  cur = con.execute('SELECT DISTINCT description, trail , img, difficulty, distance, style, location FROM rides WHERE trail="Wakamarina track"')
+
+  for row in cur:
+    ride.append(list(row))
+
+  return render_template('result.html', ride=ride)
+
+
+
+@app.route('/Coronetpeak/result')
+def coropeak():
+  ride = []
+  con = sqlite3.connect(MENUDB)
+  cur = con.execute('SELECT DISTINCT description, trail , img, difficulty, distance, style, location FROM rides WHERE trail="Coronet Peak"')
 
   for row in cur:
     ride.append(list(row))
@@ -720,98 +763,3 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-
-
-
-
-'''
-def fetchMenu(con):
-  burgers = []
-  free = '0'
-  cur = con.execute('SELECT burger,price FROM burgers WHERE price>=?', (free,))
-  for row in cur:
-    burgers.append(list(row))
-
-  drinks = []
-  cur = con.execute('SELECT drink,price FROM drinks')
-  for row in cur:
-    drinks.append(list(row))
-
-  sides = []
-  cur = con.execute('SELECT side,price FROM sides')
-  for row in cur:
-    sides.append(list(row))
-
-  return {'burgers':burgers, 'drinks':drinks, 'sides':sides}
-
-@app.route('/')
-def index():
-  con = sqlite3.connect(MENUDB)
-  menu = fetchMenu(con)
-  con.close()
-  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=menu['burgers'], drinks=menu['drinks'], sides=menu['sides'])
-
-@app.route('/order')
-def order():
-  con = sqlite3.connect(MENUDB)
-  menu = fetchMenu(con)
-  con.close()
-  return render_template('order.html', burgers=menu['burgers'], drinks=menu['drinks'], sides=menu['sides'])
-
-@app.route('/confirm', methods=['POST'])
-def confirm():
-  details = {}
-  items = {}
-
-  for input in request.form:
-    if input == 'name' or input == 'address':
-      details[input] = request.form[input]
-    elif request.form[input] and request.form[input] != '0':
-      items[input] = request.form[input]
-
-  con = sqlite3.connect(MENUDB)
-  cur = con.execute( 'INSERT INTO orders(name, address, items) VALUES(?, ?, ?)', (details['name'], details['address'], str(items)) )
-  con.commit()
-  con.close()
-
-  return render_template('confirm.html', details=details, items=items)
-
-@app.route('/vieworder/<order_id>')
-def viewOrder(order_id):
-  if 'username' in session:
-    con = sqlite3.connect(MENUDB)
-    cur = con.execute('SELECT * FROM orders WHERE id=?', (order_id,))
-    order = cur.fetchone()
-    con.close()
-    return str(order) + ' user: ' + session['username']
-  else:
-    return redirect(url_for('login')) #render_template('login.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  if request.method == 'POST' and request.form['username'] == 'admin':
-    session['username'] = request.form['username']
-    return redirect(url_for('panel'))
-  else:
-    return render_template('login.html')
-
-@app.route('/panel')
-def panel():
-  orders = []
-  if 'username' in session:
-    con = sqlite3.connect(MENUDB)
-    cur = con.execute('SELECT * FROM orders')
-    for row in cur:
-      orders.append(list(row))
-    con.close()
-    return render_template('panel.html', orders=orders)
-  else:
-    return render_template('login.html')
-
-@app.route('/logout')
-def logout():
-  if 'username' in session:
-    session.pop('username', None)
-  return redirect(url_for('index'))
-'''
